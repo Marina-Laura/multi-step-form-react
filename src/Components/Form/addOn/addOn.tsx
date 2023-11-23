52bf611274d2be13e8f910a76ca6423a3f08b31f
Laura <neel.laura96@gmail.com>
2023-11-23 09:34:44 -0500

    Initial commit

---
+++ src/Components/Form/addOn/addOn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const addOn = () => {
+  return <div></div>;
+};
+
+export default addOn;
---
+++ src/FormData.ts
@@ -0,0 +1,99 @@
+export const stepData = {
+  1: {
+    stepName: "YOUR INFO",
+    title: "Personal Info",
+    subtitle: "Please provide your name, email address and phone number",
+    content: [
+      {
+        title: "Name",
+        type: "input",
+        placeholder: "e.g. Stephen King",
+        validation: "",
+        formatting: "",
+      },
+      {
+        title: "Email Address",
+        type: "input",
+        placeholder: "e.g. stephenking@lorem.com",
+        validation: "",
+        formatting: "",
+      },
+      {
+        title: "Phone Number",
+        type: "input",
+        placeholder: "e.g. +1 234 567 890",
+        validation: "",
+        formatting: "",
+      },
+    ],
+  },
+  2: {
+    stepName: "SELECT PLAN",
+    title: "Select your plan",
+    subtitle: "You have the option of monthly or yearly billing.",
+    content: [
+      {
+        title: "Arcade",
+        type: "product",
+        price_monthly: 9,
+        price_yearly: 90,
+        icon: "",
+      },
+      {
+        title: "Advanced",
+        type: "product",
+        price_monthly: 12,
+        price_yearly: 120,
+        icon: "",
+      },
+      {
+        title: "Pro",
+        type: "product",
+        price_monthly: 15,
+        price_yearly: 150,
+        icon: "",
+      },
+      {
+        type: "switch",
+      },
+    ],
+  },
+  3: {
+    stepName: "ADD-ONS",
+    title: "Pick add-ons",
+    subtitle: "Add-ons help enhance your gaming experience.",
+    content: [
+      {
+        title: "Online service",
+        type: "add-on",
+        subtitle: "Access to multiplayer games",
+        price_monthly: 1,
+        price_yearly: 10,
+      },
+      {
+        title: "Larger Storage",
+        type: "add-on",
+        subtitle: "Extra 1TB of cloud drive",
+        price_monthly: 2,
+        price_yearly: 20,
+      },
+      {
+        title: "Customizable profile",
+        type: "add-on",
+        subtitle: "Custom theme on your profile",
+        price_monthly: 2,
+        price_yearly: 20,
+      },
+    ],
+  },
+  4: {
+    stepName: "SUMMARY",
+    title: "Finishing up",
+    subtitle: "Double check everything looks OK before confirming.",
+    content: [
+      {
+        type: "summary",
+      },
+    ],
+  },
+};
